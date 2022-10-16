f3db889650379257b081fa0ea8a7b6b4d08840d1
Elliot-Ezekiel Koh <elliotkoh@Elliot-Ezekiels-MacBook-Pro.local>
2022-10-16 21:41:22 +0800

    add meta tags

--- src/root.tsx
+++ src/root.tsx
@@ -18,9 +18,15 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>Nashville Guitar Scales</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta name="author" content="Elliot Koh" />
+        <Meta
+          name="description"
+          content="Nashville guitar scales is a way to explore chord shapes and scales using the nashville numbering system"
+        />
+
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7821046190013944"
